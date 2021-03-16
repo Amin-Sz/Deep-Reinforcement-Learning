68631c6eed91214905b339dd0928d164eb5495f0
Mohammad Amin Soleimanzade <79352270+Amin-Sz@users.noreply.github.com>
2021-03-15 20:48:12 -0600

    Created the "main" function.

--- Section 4 - DDPG/DDPG - MountainCarContinuous.py
+++ Section 4 - DDPG/DDPG - MountainCarContinuous.py
@@ -45,19 +45,22 @@ class ReplayBuffer:
 
 
 class Agent:
-    def __init__(self, mem_max_size, lr_Q, lr_policy, state_size, action_size, Q_layers, policy_layers, tau):
+    def __init__(self, mem_max_size, lr_Q, lr_policy, state_size, action_size, action_max, action_min, Q_layers,
+                 policy_layers, tau):
         self.replay_buffer = ReplayBuffer(max_size=mem_max_size)
         self.tau = tau
         self.action_size = action_size
-        self.Q_network = ann(input_size=int(state_size + action_size), layers=Q_layers)  # CHECK THE LOSS FUNCTION
-        self.Q_network.compile(optimizer=Adam(learning_rate=lr_Q), loss='mean_squared_error')
-        self.policy_network = ann(input_size=state_size, layers=policy_layers)
-        self.policy_network.compile(optimizer=Adam(learning_rate=lr_policy))
+        self.action_min = action_min
+        self.action_max = action_max
+        self.critic_network = ann(input_size=int(state_size + action_size), layers=Q_layers)
+        self.critic_network.compile(optimizer=Adam(learning_rate=lr_Q), loss='mean_squared_error')
+        self.actor_network = ann(input_size=state_size, layers=policy_layers)
+        self.actor_network.compile(optimizer=Adam(learning_rate=lr_policy))
 
-        self.Q_network_target = ann(input_size=int(state_size + action_size), layers=Q_layers)  # CHECK THESE
-        self.Q_network_target.compile(optimizer=Adam(learning_rate=lr_Q))
-        self.policy_network_target = ann(input_size=state_size, layers=policy_layers)
-        self.policy_network_target.compile(optimizer=Adam(learning_rate=lr_policy))
+        self.critic_network_target = ann(input_size=int(state_size + action_size), layers=Q_layers)
+        self.critic_network_target.compile(optimizer=Adam(learning_rate=lr_Q))
+        self.actor_network_target = ann(input_size=state_size, layers=policy_layers)
+        self.actor_network_target.compile(optimizer=Adam(learning_rate=lr_policy))
 
     def update_networks(self, scaled_states, actions, rewards, scaled_next_states, dones, gamma):
         scaled_states = tf.convert_to_tensor(scaled_states, dtype=tf.float32)
@@ -68,42 +71,45 @@ class Agent:
         dones = dones.astype(int)
 
         X_Q = Concatenate()([scaled_states, actions])
-        Y_Q = rewards + gamma*(1 - dones)*self.get_Q_target(scaled_next_states)
-        self.Q_network.fit(X_Q, Y_Q, verbose=0)
+        Y_Q = rewards + gamma * (1 - dones) * self.get_critic_target(scaled_next_states)
+        self.critic_network.fit(X_Q, Y_Q, verbose=0)
 
         with tf.GradientTape() as tape:
-            mu = self.policy_network.call(scaled_states)  # This should be different than actions (due to noise)
+            mu = self.actor_network.call(scaled_states)  # This should be different than actions (due to noise)
             x = Concatenate()([scaled_states, mu])
-            loss_value = -tf.reduce_mean(self.Q_network.call(x))
-        gradients = tape.gradient(loss_value, self.policy_network.trainable_variables)
-        self.policy_network.optimizer.apply_gradients(zip(gradients, self.policy_network.trainable_variables))
+            loss_value = -tf.reduce_mean(self.critic_network.call(x))
+        gradients = tape.gradient(loss_value, self.actor_network.trainable_variables)
+        self.actor_network.optimizer.apply_gradients(zip(gradients, self.actor_network.trainable_variables))
 
-    def update_Q_target(self):
+    def update_critic_target(self):
         variables = []
-        for i in range(len(self.Q_network_target.weights)):
-            variables.append(self.Q_network_target.weights[i]*self.tau +
-                             self.Q_network.weights[i]*(1 - self.tau))
-        self.Q_network_target.set_weights(variables)
+        for i in range(len(self.critic_network_target.weights)):
+            variables.append(self.critic_network_target.weights[i] * self.tau +
+                             self.critic_network.weights[i] * (1 - self.tau))
+        self.critic_network_target.set_weights(variables)
 
-    def update_policy_target(self):
+    def update_actor_target(self):
         variables = []
-        for i in range(len(self.policy_network_target.weights)):
-            variables.append(self.policy_network_target.weights[i]*self.tau +
-                             self.policy_network.weights[i]*(1 - self.tau))
-        self.policy_network_target.set_weights(variables)
+        for i in range(len(self.actor_network_target.weights)):
+            variables.append(self.actor_network_target.weights[i] * self.tau +
+                             self.actor_network.weights[i] * (1 - self.tau))
+        self.actor_network_target.set_weights(variables)
 
-    def get_action(self, scaled_state):
+    def get_action(self, scaled_state, training=True):
         scaled_state = tf.convert_to_tensor(scaled_state, dtype=tf.float32)
-        action = self.policy_network.call(scaled_state) + tf.random.normal(shape=[self.action_size],
-                                                                           mean=0.0, stddev=0.1)
-        action = tf.clip_by_value(action, clip_value_min=-1, clip_value_max=1)
+        if training:
+            action = self.actor_network.call(scaled_state)*self.action_max + tf.random.normal(shape=[self.action_size],
+                                                                                              mean=0.0, stddev=0.1)
+        else:
+            action = self.actor_network.call(scaled_state)*self.action_max
+        action = tf.clip_by_value(action, clip_value_min=self.action_min, clip_value_max=self.action_max)
         action = tf.squeeze(action)
         return action
 
-    def get_Q_target(self, scaled_state):
-        mu = self.policy_network_target.call(scaled_state)
+    def get_critic_target(self, scaled_state):
+        mu = self.actor_network_target.call(scaled_state)
         x = Concatenate()([scaled_state, mu])
-        return tf.squeeze(self.Q_network_target.call(x))
+        return tf.squeeze(self.critic_network_target.call(x))
 
 
 def ann(input_size, layers=[(10, 'relu')]):
@@ -116,10 +122,6 @@ def ann(input_size, layers=[(10, 'relu')]):
     return model
 
 
-def squared_error(y_true, y_predict):
-    return tf.reduce_sum(tf.square(y_predict - y_true), axis=-1)
-
-
 def get_scaler(env):
     scaler = StandardScaler()
     x = []
@@ -130,80 +132,96 @@ def get_scaler(env):
     return scaler
 
 
-def play_one_game(env, scaler):  # CHECK THIS (that agent gets updated)
+def play_one_game(agent, env, scaler):
     observation = env.reset()
     done = False
     counter = 0
     total_reward = 0
     while not done:
-        a = agent.get_action(scaler.transform([observation]))  # CHECK THIS
+        a = agent.get_action(scaler.transform([observation]))
         prev_observation = observation
-        observation, reward, done, info = env.step([a])  # CHECK THIS
+        observation, reward, done, info = env.step([a])
         total_reward = total_reward + reward
         counter = counter + 1
         if counter >= 2000:
             done = True
         agent.replay_buffer.add_to_mem(prev_observation, a, reward, observation, done)
-    return total_reward, counter
+    return agent, total_reward, counter
 
 
-# Creating the scaler, agent and environment
-env = gym.make('MountainCarContinuous-v0')
-scaler = get_scaler(env)
-max_buffer_size = int(1e6)
-agent = Agent(mem_max_size=max_buffer_size, lr_Q=0.001, lr_policy=0.001, state_size=env.observation_space.shape[0],
-              action_size=env.action_space.shape[0], Q_layers=[(512, 'relu'), (1, 'linear')],
-              policy_layers=[(512, 'relu'), (env.action_space.shape[0], 'tanh')], tau=0.995)
+def main(training=False):
+    # Creating the scaler, agent and environment
+    env = gym.make('MountainCarContinuous-v0')
+    scaler = get_scaler(env)
+    max_buffer_size = int(1e6)
+    agent = Agent(mem_max_size=max_buffer_size, lr_Q=0.001, lr_policy=0.001, state_size=env.observation_space.shape[0],
+                  action_size=env.action_space.shape[0], action_max=env.action_space.high[0],
+                  action_min=env.action_space.low[0], Q_layers=[(512, 'relu'), (1, 'linear')],
+                  policy_layers=[(512, 'relu'), (env.action_space.shape[0], 'tanh')], tau=0.995)
+
+    if training:
+        # Training
+        num_iteration = 400
+        min_buffer_size = 10000
+        batch_size = 100
+        gamma = 0.99  # Discount factor
+        reward_set = []  # Stores rewards of each episode
+        avg_reward_set = []  # Stores the average of the last 100 rewards
+
+        for t in range(num_iteration):
+            agent, total_reward, counter = play_one_game(agent, env, scaler)
+
+            if agent.replay_buffer.mem_size >= min_buffer_size:
+                for j in range(counter):
+                    s, a, r, s2, done = agent.replay_buffer.sample(batch_size=batch_size)
+                    agent.update_networks(scaler.transform(s), a, r, scaler.transform(s2), done, gamma=gamma)
+                    agent.update_critic_target()
+                    agent.update_actor_target()
+
+            reward_set.append(total_reward)
+            avg_reward_set.append(np.mean(reward_set[-100:]))
+            if (t + 1) % 20 == 0 or t == 0:
+                print('iteration #' + str(t + 1), '--->', 'total reward:' + '%.2f' % total_reward + ', ',
+                      'averaged reward:' + '%.2f' % np.mean(reward_set[-100:]))
+
+        # Plotting the train results
+        axes = plt.axes()
+        axes.set_ylim([np.min(reward_set) - 400, np.max(reward_set) + 50])
+        plt.xlabel('Episode')
+        plt.ylabel('Reward')
+        plt.plot(np.arange(1, num_iteration + 1), reward_set)
+        plt.plot(np.arange(1, num_iteration + 1), avg_reward_set)
+        legend_2 = 'Running average of the last 100 episodes (' + '%.2f' % np.mean(reward_set[-100:]) + ')'
+        plt.legend(['Reward', legend_2], loc=4)
+        plt.show()
+        # plt.savefig('Section 4 - DDPG/Pendulum-v0/Rewards_pendulum')
+
+        # Saving the networks
+        '''agent.actor_network.save('Section 4 - DDPG/Pendulum-v0/actor_pendulum.h5')
+        agent.critic_network.save('Section 4 - DDPG/Pendulum-v0/critic_pendulum.h5')
+        agent.actor_network_target.save('Section 4 - DDPG/Pendulum-v0/actor_target_pendulum.h5')
+        agent.critic_network_target.save('Section 4 - DDPG/Pendulum-v0/critic_target_pendulum.h5')'''
+
+    else:
+        # Importing the trained networks
+        '''agent.actor_network = tf.keras.models.load_model('Section 4 - DDPG/Pendulum-v0/actor_pendulum.h5')
+        agent.actor_network_target = tf.keras.models.load_model('Section 4 - DDPG/Pendulum-v0/actor_target_pendulum.h5')
+        agent.critic_network = tf.keras.models.load_model('Section 4 - DDPG/Pendulum-v0/critic_pendulum.h5')
+        agent.critic_network_target = tf.keras.models.load_model('Section 4 - DDPG/Pendulum-v0/critic_target_pendulum.h5')'''
+
+        # Showing the video
+        observation = env.reset()
+        done = False
+        total_reward = 0
+        while not done:
+            env.render()
+            a = agent.get_action(scaler.transform([observation]), training=False)
+            observation, reward, done, info = env.step([a])
+            total_reward = total_reward + reward
+        env.close()
+        print('total reward:' + '%.2f' % total_reward)
 
 
-# Training
-num_iteration = 250
-min_buffer_size = 10000
-batch_size = 100
-gamma = 0.99  # Discount factor
-reward_set = []
-avg_reward_set = []
-counter_set = []
-
-for t in range(num_iteration):
-    total_reward, counter = play_one_game(env, scaler)
-
-    if agent.replay_buffer.mem_size >= min_buffer_size:
-        for j in range(counter):
-            s, a, r, s2, done = agent.replay_buffer.sample(batch_size=batch_size)  # CHECK THIS
-            agent.update_networks(scaler.transform(s), a, r, scaler.transform(s2), done, gamma=gamma)
-            agent.update_Q_target()
-            agent.update_policy_target()
-
-    counter_set.append(counter)
-    reward_set.append(total_reward)
-    avg_reward_set.append(np.mean(reward_set[-100:]))
-    if t % 1 == 0:
-        print('iteration #' + str(t + 1), '--->', 'total reward:' + '%.3f' % total_reward + ', ',
-              'averaged reward:' + '%.3f' % np.mean(reward_set[-100:]))
-
-
-# Plotting the results
-plt.plot(reward_set)
-plt.plot(avg_reward_set)
-plt.plot(counter_set)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main(training=True)
 
