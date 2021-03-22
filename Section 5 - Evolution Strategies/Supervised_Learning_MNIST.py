92120cc37b03629497a6b00edcfb3ccdf82dd846
Mohammad Amin Soleimanzade <79352270+Amin-Sz@users.noreply.github.com>
2021-03-22 02:20:45 -0600

    Completed the Supervised_Learning_MNIST.py file.

--- Section 5 - Evolution Strategies/Supervised_Learning_MNIST.py
+++ Section 5 - Evolution Strategies/Supervised_Learning_MNIST.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import keras
+from keras.utils import to_categorical
+import multiprocessing
+from multiprocessing.dummy import Pool
 
 
 def softmax(x):
@@ -23,11 +27,16 @@ class ANN:
         self.parameters = [self.w1, self.b1, self.w2, self.b2]
 
     def set_parameters(self, params):
-        self.parameters = params
+        I, H, O = self.input_size, self.hidden_size, self.output_size
+        w1 = params[0: I * H].reshape((I, H))
+        b1 = params[I * H: I * H + H]
+        w2 = params[I * H + H: I * H + H + H * O].reshape((H, O))
+        b2 = params[I * H + H + H * O:]
+        self.parameters = [w1, b1, w2, b2]
 
     def get_parameters(self):
         return np.concatenate([self.parameters[0].flatten(), self.parameters[1].flatten(), self.parameters[2].flatten(),
-                              self.parameters[3].flatten()])
+                               self.parameters[3].flatten()])
 
     def predict(self, x):  # (batch_size, n_features)
         x = x.dot(self.parameters[0]) + self.parameters[1]
@@ -39,6 +48,77 @@ class ANN:
         y_predict = self.predict(x)
         y_predict = np.argmax(y_predict, axis=1)
         return np.mean(y_predict == y)
+
+
+def calculate_reward(parameters):
+    ann = ANN(I, H, O)
+    ann.set_parameters(parameters)
+    return ann.score(x_train, y_train)
+
+
+def evolution_strategy(lr, sigma, population_size, model, f, n_iteration):
+    n_parameters = len(model.get_parameters())
+    rewards_train = np.zeros(n_iteration)
+    rewards_test = np.zeros(n_iteration)
+
+    for t in range(n_iteration):
+        epsilon = np.random.randn(population_size, n_parameters)
+        rewards = pool(f, [model.get_parameters() + sigma*epsilon[i, :] for i in range(population_size)])
+        mean = np.mean(rewards)
+        std = np.std(rewards)
+        assert(len(mean) == 1)
+        rewards_standardized = (rewards - mean)/std
+        updated_parameters = model.get_parameters() + lr*rewards_standardized.dot(epsilon)/(population_size*sigma)
+        model.set_parameters(updated_parameters)
+
+        rewards_train[t] = mean
+        test_reward = model.score(x_test, y_test)
+        rewards_test[t] = test_reward
+        if (t + 1) % 20 == 0 or t == 0:
+            print('iteration #' + str(t + 1) + ' ---> train accuracy:' + '%.2f' % mean + ', test accuracy: ' +
+                  '%.2f' % test_reward)
+
+    return model, reward_train, reward_test
+
+
+if __name__ == '__main__':
+    pool = Pool(multiprocessing.cpu_count() - 1)
+
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    x_train, x_test = x_train/255, x_test/255
+    y_train, y_test = to_categorical(y_train), to_categorical(y_test)
+
+    I = len(x_train[0].flatten())
+    H = 128
+    O = len(y_train[0])
+    learning_rate = 0.001
+    noise_std = 0.1
+    population_size = 500
+    n_iteration = 500
+    network = ANN(I, H, O)
+
+    network, reward_train, reward_test = evolution_strategy(lr=learning_rate, sigma=noise_std,
+                                                            population_size=population_size, model=network,
+                                                            f=calculate_reward, n_iteration=n_iteration)
+
+    # Plotting the results
+    plt.xlabel('Iteration')
+    plt.ylabel('Accuracy')
+    plt.plot(np.arange(1, n_iteration + 1), reward_train)
+    plt.plot(np.arange(1, n_iteration + 1), reward_test)
+    plt.legend(['Train', 'Test'], loc=4)
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
