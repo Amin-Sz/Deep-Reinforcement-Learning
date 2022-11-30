4faef9937ef1e83c58cff578beb17a6c9a92d535
Amin-Sz <am.soleimanzade@gmail.com>
2022-11-29 19:33:01 -0700

    changed dir_

--- PPO/PPO_Acrobot-v1.py
+++ PPO/PPO_Acrobot-v1.py
@@ -221,7 +221,7 @@ def play_one_episode(agent, env):
 
 def main(training=True):
     env = gym.make('Acrobot-v1')
-    dir_ = 'Section 5 - PPO/Acrobot-v1'
+    dir_ = 'Acrobot-v1'
 
     batch_size = 5
     gamma = 0.99
