cf73e70e79d30088b0942f836d3eec23286f546a
Amin-Sz <am.soleimanzade@gmail.com>
2022-12-01 19:52:38 -0700

    changed the directory for saving/loading model.

--- LSTM-TD3/LSTM-TD3_AntPyBulletEnv-v0.py
+++ LSTM-TD3/LSTM-TD3_AntPyBulletEnv-v0.py
@@ -383,24 +383,24 @@ def main(training=True):
         legend_2 = 'Running average of the last 100 episodes (' + '%.2f' % np.mean(reward_set[-100:]) + ')'
         plt.legend(['Reward', legend_2], loc=4)
         plt.show()
-        plt.savefig('Section 8 - LSTM-TD3/AntPyBulletEnv-v0/Rewards_AntPyBulletEnv')
+        plt.savefig('AntPyBulletEnv-v0/Rewards_AntPyBulletEnv')
 
         # Saving the networks
-        T.save(agent.critic_1.state_dict(), 'Section 8 - LSTM-TD3/AntPyBulletEnv-v0/critic_1')
-        T.save(agent.critic_2.state_dict(), 'Section 8 - LSTM-TD3/AntPyBulletEnv-v0/critic_2')
-        T.save(agent.actor.state_dict(), 'Section 8 - LSTM-TD3/AntPyBulletEnv-v0/actor')
-        T.save(agent.target_critic_1.state_dict(), 'Section 8 - LSTM-TD3/AntPyBulletEnv-v0/target_critic_1')
-        T.save(agent.target_critic_2.state_dict(), 'Section 8 - LSTM-TD3/AntPyBulletEnv-v0/target_critic_2')
-        T.save(agent.target_actor.state_dict(), 'Section 8 - LSTM-TD3/AntPyBulletEnv-v0/target_actor')
+        T.save(agent.critic_1.state_dict(), 'AntPyBulletEnv-v0/critic_1')
+        T.save(agent.critic_2.state_dict(), 'AntPyBulletEnv-v0/critic_2')
+        T.save(agent.actor.state_dict(), 'AntPyBulletEnv-v0/actor')
+        T.save(agent.target_critic_1.state_dict(), 'AntPyBulletEnv-v0/target_critic_1')
+        T.save(agent.target_critic_2.state_dict(), 'AntPyBulletEnv-v0/target_critic_2')
+        T.save(agent.target_actor.state_dict(), 'AntPyBulletEnv-v0/target_actor')
 
     else:
         # Loading the trained networks
-        agent.critic_1.load_state_dict(T.load('Section 8 - LSTM-TD3/AntPyBulletEnv-v0/critic_1'))
-        agent.critic_2.load_state_dict(T.load('Section 8 - LSTM-TD3/AntPyBulletEnv-v0/critic_2'))
-        agent.actor.load_state_dict(T.load('Section 8 - LSTM-TD3/AntPyBulletEnv-v0/actor'))
-        agent.target_critic_1.load_state_dict(T.load('Section 8 - LSTM-TD3/AntPyBulletEnv-v0/target_critic_1'))
-        agent.target_critic_2.load_state_dict(T.load('Section 8 - LSTM-TD3/AntPyBulletEnv-v0/target_critic_2'))
-        agent.target_actor.load_state_dict(T.load('Section 8 - LSTM-TD3/AntPyBulletEnv-v0/target_actor'))
+        agent.critic_1.load_state_dict(T.load('AntPyBulletEnv-v0/critic_1'))
+        agent.critic_2.load_state_dict(T.load('AntPyBulletEnv-v0/critic_2'))
+        agent.actor.load_state_dict(T.load('AntPyBulletEnv-v0/actor'))
+        agent.target_critic_1.load_state_dict(T.load('AntPyBulletEnv-v0/target_critic_1'))
+        agent.target_critic_2.load_state_dict(T.load('AntPyBulletEnv-v0/target_critic_2'))
+        agent.target_actor.load_state_dict(T.load('AntPyBulletEnv-v0/target_actor'))
 
         # Showing the video
         env.render(mode='human')
