fcdc3f58397fa9613f63cc8bfa23588960ed2c74
Amin-Sz <am.soleimanzade@gmail.com>
2022-12-01 19:58:45 -0700

    changed the directory for saving/loading model.

--- LSTM-TD3/LSTM-TD3_InvertedDoublePendulumPyBulletEnv-v0.py
+++ LSTM-TD3/LSTM-TD3_InvertedDoublePendulumPyBulletEnv-v0.py
@@ -377,30 +377,30 @@ def main(training=True):
         legend_2 = 'Running average of the last 100 episodes (' + '%.2f' % np.mean(reward_set[-100:]) + ')'
         plt.legend(['Reward', legend_2], loc=4)
         plt.show()
-        plt.savefig('Section 8 - LSTM-TD3/InvertedDoublePendulumPyBulletEnv-v0/'
+        plt.savefig('InvertedDoublePendulumPyBulletEnv-v0/'
                     'Rewards_InvertedDoublePendulumPyBulletEnv')
 
         # Saving the networks
-        T.save(agent.critic_1.state_dict(), 'Section 8 - LSTM-TD3/InvertedDoublePendulumPyBulletEnv-v0/critic_1')
-        T.save(agent.critic_2.state_dict(), 'Section 8 - LSTM-TD3/InvertedDoublePendulumPyBulletEnv-v0/critic_2')
-        T.save(agent.actor.state_dict(), 'Section 8 - LSTM-TD3/InvertedDoublePendulumPyBulletEnv-v0/actor')
-        T.save(agent.target_critic_1.state_dict(), 'Section 8 - LSTM-TD3/InvertedDoublePendulumPyBulletEnv-v0/'
+        T.save(agent.critic_1.state_dict(), 'InvertedDoublePendulumPyBulletEnv-v0/critic_1')
+        T.save(agent.critic_2.state_dict(), 'InvertedDoublePendulumPyBulletEnv-v0/critic_2')
+        T.save(agent.actor.state_dict(), 'InvertedDoublePendulumPyBulletEnv-v0/actor')
+        T.save(agent.target_critic_1.state_dict(), 'InvertedDoublePendulumPyBulletEnv-v0/'
                                                    'target_critic_1')
-        T.save(agent.target_critic_2.state_dict(), 'Section 8 - LSTM-TD3/InvertedDoublePendulumPyBulletEnv-v0/'
+        T.save(agent.target_critic_2.state_dict(), 'InvertedDoublePendulumPyBulletEnv-v0/'
                                                    'target_critic_2')
-        T.save(agent.target_actor.state_dict(), 'Section 8 - LSTM-TD3/InvertedDoublePendulumPyBulletEnv-v0/'
+        T.save(agent.target_actor.state_dict(), 'InvertedDoublePendulumPyBulletEnv-v0/'
                                                 'target_actor')
 
     else:
         # Loading the trained networks
-        agent.critic_1.load_state_dict(T.load('Section 8 - LSTM-TD3/InvertedDoublePendulumPyBulletEnv-v0/critic_1'))
-        agent.critic_2.load_state_dict(T.load('Section 8 - LSTM-TD3/InvertedDoublePendulumPyBulletEnv-v0/critic_2'))
-        agent.actor.load_state_dict(T.load('Section 8 - LSTM-TD3/InvertedDoublePendulumPyBulletEnv-v0/actor'))
-        agent.target_critic_1.load_state_dict(T.load('Section 8 - LSTM-TD3/InvertedDoublePendulumPyBulletEnv-v0/'
+        agent.critic_1.load_state_dict(T.load('InvertedDoublePendulumPyBulletEnv-v0/critic_1'))
+        agent.critic_2.load_state_dict(T.load('InvertedDoublePendulumPyBulletEnv-v0/critic_2'))
+        agent.actor.load_state_dict(T.load('InvertedDoublePendulumPyBulletEnv-v0/actor'))
+        agent.target_critic_1.load_state_dict(T.load('InvertedDoublePendulumPyBulletEnv-v0/'
                                                      'target_critic_1'))
-        agent.target_critic_2.load_state_dict(T.load('Section 8 - LSTM-TD3/InvertedDoublePendulumPyBulletEnv-v0/'
+        agent.target_critic_2.load_state_dict(T.load('InvertedDoublePendulumPyBulletEnv-v0/'
                                                      'target_critic_2'))
-        agent.target_actor.load_state_dict(T.load('Section 8 - LSTM-TD3/InvertedDoublePendulumPyBulletEnv-v0/'
+        agent.target_actor.load_state_dict(T.load('InvertedDoublePendulumPyBulletEnv-v0/'
                                                   'target_actor'))
 
         # Showing the video
